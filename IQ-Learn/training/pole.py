610a776b05e261eaf83ca7ac3313db94e2e87ea7
Susanna Di Vita <susanna.divita.2@gmail.com>
2023-05-31 20:43:59 +0200

    Generate expert

--- IQ-Learn/training/pole.py
+++ IQ-Learn/training/pole.py
@@ -1,43 +1,26 @@
 import gym
-
 from stable_baselines3 import DQN
-from stable_baselines3.common.evaluation import evaluate_policy
 
-
-# Create environment
 env = gym.make("LunarLander-v2")
+env.reset(seed=69)
 
-# # Instantiate the agent
-# model = DQN("MlpPolicy", env, verbose=1)
-# # Train the agent and display a progress bar
-# model.learn(total_timesteps=int(2e5), progress_bar=True)
-# # Save the agent
-# model.save("dqn_lunar")
-# del model  # delete trained model to demonstrate loading
+model = DQN("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=100_000, log_interval=10, progress_bar=True)
+model.save("dqn_lunar")
 
-# Load the trained agent
-# NOTE: if you have loading issue, you can pass `print_system_info=True`
-# to compare the system on which the model was trained vs the current one
-# model = DQN.load("dqn_lunar", env=env, print_system_info=True)
-model = DQN.load("dqn_lunar", env=env)
+env = gym.make("LunarLander-v2")
+env.reset(seed=42)
+model = DQN.load("dqn_lunar", env)
 
-# Evaluate the agent
-# NOTE: If you use wrappers with your environment that modify rewards,
-#       this will be reflected here. To evaluate with original rewards,
-#       wrap environment in a "Monitor" wrapper before other wrappers.
-mean_reward, std_reward = evaluate_policy(
-    model, model.get_env(), n_eval_episodes=10)
-del model
+NUM_SAMPLES = 10
 
-print(mean_reward, std_reward)
+# # Evaluate the agent
+for i_sample in range(NUM_SAMPLES):
 
-env_render = gym.make("LunarLander-v2", render_mode="human")
-model = DQN.load("dqn_lunar", env=env_render)
-
-# Enjoy trained agent
-vec_env = model.get_env()
-obs = vec_env.reset()
-for i in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, rewards, dones, info = vec_env.step(action)
-    vec_env.render()
+    vec_env = model.get_env()
+    obs = vec_env.reset()
+    for i in range(1000):
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward, done, info = vec_env.step(action)
+        vec_env.render()
+    print(f"Sample {i_sample}", reward)
